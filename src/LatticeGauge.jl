709f6d73b599ec7b4c6091775681e1923121f223
nfphys <fumikun0102@me.com>
2021-09-15 19:26:44 +0900

    mainとplotを統一した

--- src/LatticeGauge.jl
+++ src/LatticeGauge.jl
@@ -45,9 +45,16 @@ function measure_observables!(Us, param, β; random=false)
 end
 
 
-function main_U1(param, βs)
-    @unpack Nsite = param
-    Us = ones(ComplexF64, Nsite, Nsite, Nsite, Nsite, 4)
+
+
+function main!(Us, param, βs)
+    @unpack Nsite, dim = param
+
+    @assert ndims(Us) === dim+1
+    for i in 1:dim 
+        @assert size(Us, i) === Nsite 
+    end
+    @assert size(Us, dim+1) === dim 
 
     Ps_ordered = zeros(Float64, length(βs))
     ΔPs_ordered = zeros(Float64, length(βs))
@@ -70,7 +77,13 @@ function main_U1(param, βs)
         ΔPs_random[iβ] = ΔP
     end
 
-    result = (βs=βs, 
+    group = :SU2 
+    if typeof(Us) == Array{ComplexF64, dim+1} 
+        group = :U1
+    end
+
+    result = (dim=dim, βs=βs, 
+        group=group,
         Ps_ordered=Ps_ordered, 
         ΔPs_ordered=ΔPs_ordered, 
         Ps_random=Ps_random, 
@@ -79,168 +92,61 @@ function main_U1(param, βs)
     return result 
 end
 
-
-function plot_U1(result; xlog=true)
-    @unpack βs, Ps_ordered, ΔPs_ordered, Ps_random, ΔPs_random = result 
-
-    # plot average plaquette 
-    p = plot(xlabel="β", yaxis=:log, ylabel="P", legend=:bottomleft)
-    if xlog 
-        plot!(xaxis=:log)
-    end
-    plot!(p, βs, Ps_ordered; marker=:dot, line=false, label="ordered start")
-    plot!(p, βs, Ps_random; marker=:dot, line=false, label="random start")
-
-    f(x) = ifelse(x<1.5, 1-x/2, NaN)
-    plot!(p, βs, f.(βs), label="strong coupling limit: 1 - β/2")
-
-    g(x) = ifelse(x>0.5, 1/4x,  NaN)
-    plot!(p, βs, g.(βs), label="weak coupling limit: 1/4β")
-    display(p)
-
-    # plot fluctuation of plaquette 
-    p = plot(xlabel="β", ylabel="ΔP")
-    if xlog 
-        plot!(xaxis=:log)
-    end
-    plot!(p, βs, ΔPs_ordered; marker=:dot, line=false, label="ordered start")
-    plot!(p, βs, ΔPs_random; marker=:dot, line=false, label="random start")
-    display(p)
-end
-
-
-
-
-
-function main_SU2(param, βs)
-    @unpack Nsite = param
-    Us = Array{SU2}(undef, Nsite, Nsite, Nsite, Nsite, 4)
-
-    Ps_ordered = zeros(Float64, length(βs))
-    ΔPs_ordered = zeros(Float64, length(βs))
-
-    Ps_random = zeros(Float64, length(βs))
-    ΔPs_random = zeros(Float64, length(βs))
-
-    for iβ in 1:length(βs)
-        β = βs[iβ]
-
-        # ordered start
-        @time P, ΔP = measure_observables!(Us, param, β; random=false)
-
-        Ps_ordered[iβ] = P
-        ΔPs_ordered[iβ] = ΔP
-
-        # random start
-        @time P, ΔP = measure_observables!(Us, param, β; random=true)
-        Ps_random[iβ] = P
-        ΔPs_random[iβ] = ΔP
-    end
-
-    result = (βs=βs, 
-        Ps_ordered=Ps_ordered, 
-        ΔPs_ordered=ΔPs_ordered, 
-        Ps_random=Ps_random, 
-        ΔPs_random=ΔPs_random)
-
-    return result 
-end
-
-function plot_SU2(result; xlog=true)
-    @unpack βs, Ps_ordered, ΔPs_ordered, Ps_random, ΔPs_random = result 
+function plot_result(result; xlog=true, yerr=false, save_figure=false, figure_name="result")
+    @unpack dim, βs, group, Ps_ordered, ΔPs_ordered, Ps_random, ΔPs_random = result 
 
     # plot average plaquette
     p = plot(xlabel="β", yaxis=:log, ylabel="P", legend=:bottomleft)
     if xlog 
         plot!(xaxis=:log)
     end
-    plot!(p, βs, Ps_ordered; marker=:dot, line=false, label="ordered start")
-    plot!(p, βs, Ps_random; marker=:dot, line=false, label="random start")
+    if yerr
+        plot!(p, βs, Ps_ordered; yerr=ΔPs_ordered, marker=:dot, line=false, label="ordered start")
+        plot!(p, βs, Ps_random; yerr=ΔPs_random, marker=:dot, line=false, label="random start")
+    else
+        plot!(p, βs, Ps_ordered; marker=:dot, line=false, label="ordered start")
+        plot!(p, βs, Ps_random; marker=:dot, line=false, label="random start")
+    end
 
 
-    f(x) = ifelse(x<3.5, 1-x/4, NaN)
-    plot!(p, βs, f.(βs), label="strong coupling limit: 1 - β/4")
+    function f(x)
+        if group === :SU2 
+            return ifelse(x<3.5, 1-x/4, NaN)
+        end
+        if group === :U1
+            return ifelse(x<1.5, 1-x/2, NaN)
+        end
+        return NaN
+    end
+    plot!(p, βs, f.(βs), label=false, line=:black)
 
-    g(x) = ifelse(x>0.8, 3/4x,  NaN)
-    plot!(p, βs, g.(βs), label="weak coupling limit: 3/4β")
+    function g(x)
+        if group === :SU2 
+            return ifelse(x>0.8, 3/(dim*x),  NaN)
+        end
+        if group === :U1
+            return ifelse(x>0.5, 1/(dim*x),  NaN)
+        end
+        return NaN
+    end
+    plot!(p, βs, g.(βs), label=false, line=:black)
+    if save_figure
+        savefig("LatticeGauge_figure/average_plaquette_" * figure_name * ".png")
+    end
     display(p)
 
-    # plot fluctuation of plaquette
+    # plot plaquette fluctuation
     p = plot(xlabel="β", ylabel="ΔP", legend=:bottomleft)
     if xlog 
         plot!(xaxis=:log)
     end
     plot!(p, βs, ΔPs_ordered; marker=:dot, line=false, label="ordered start")
     plot!(p, βs, ΔPs_random; marker=:dot, line=false, label="random start")
+    if save_figure
+        savefig("LatticeGauge_figure/plaquette_fluctuation_" * figure_name * ".png")
+    end
     display(p)
 end
-
-
-
-
-function main_SU2_5d(param, βs)
-    @unpack Nsite = param
-    Us = Array{SU2}(undef, Nsite, Nsite, Nsite, Nsite, Nsite, 5)
-
-    Ps_ordered = zeros(Float64, length(βs))
-    ΔPs_ordered = zeros(Float64, length(βs))
-
-    Ps_random = zeros(Float64, length(βs))
-    ΔPs_random = zeros(Float64, length(βs))
-
-    for iβ in 1:length(βs)
-        β = βs[iβ]
-
-        # ordered start
-        @time P, ΔP = measure_observables!(Us, param, β; random=false)
-
-        Ps_ordered[iβ] = P
-        ΔPs_ordered[iβ] = ΔP
-
-        # random start
-        @time P, ΔP = measure_observables!(Us, param, β; random=true)
-        Ps_random[iβ] = P
-        ΔPs_random[iβ] = ΔP
-    end
-
-    result = (βs=βs, 
-        Ps_ordered=Ps_ordered, 
-        ΔPs_ordered=ΔPs_ordered, 
-        Ps_random=Ps_random, 
-        ΔPs_random=ΔPs_random)
-
-    return result 
-end
-
-function plot_SU2_5d(result; xlog=true)
-    @unpack βs, Ps_ordered, ΔPs_ordered, Ps_random, ΔPs_random = result 
-
-    # plot average plaquette
-    p = plot(xlabel="β", yaxis=:log, ylabel="P", legend=:bottomleft)
-    if xlog 
-        plot!(xaxis=:log)
-    end
-    plot!(p, βs, Ps_ordered; marker=:dot, line=false, label="ordered start")
-    plot!(p, βs, Ps_random; marker=:dot, line=false, label="random start")
-
-
-    f(x) = ifelse(x<3.5, 1-x/4, NaN)
-    plot!(p, βs, f.(βs), label="strong coupling limit: 1 - β/4")
-
-    g(x) = ifelse(x>0.8, 3/5x,  NaN)
-    plot!(p, βs, g.(βs), label="weak coupling limit: 3/5β")
-    display(p)
-
-    # plot fluctuation of plaquette
-    p = plot(xlabel="β", ylabel="ΔP", legend=:bottomleft)
-    if xlog 
-        plot!(xaxis=:log)
-    end
-    plot!(p, βs, ΔPs_ordered; marker=:dot, line=false, label="ordered start")
-    plot!(p, βs, ΔPs_random; marker=:dot, line=false, label="random start")
-    display(p)
-end
-
 
 
 
